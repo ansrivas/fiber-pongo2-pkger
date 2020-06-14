3cd78bd330870ca91e0235f973cc566a628554ac
Ankur Srivastava <best.ankur@gmail.com>
2020-06-14 20:53:32 +0200

    removed
    Signed-off-by: Ankur Srivastava <best.ankur@gmail.com>

--- static-build.sh
+++
@@ -1,4 +0,0 @@
-swag init --generalInfo pkg/routers/routers.go pkg/routers && \
-pkger -o web -include /web/static/ -include /web/templates/ && \
-CC=/usr/local/bin/musl-gcc CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o fiber-pongo2-pkger -a -ldflags '-extldflags "-static" -s -w'  . && \
-upx fiber-pongo2-pkger
